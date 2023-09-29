b6ac6714b5a2ea905f92c367f3a4b96725bfba15
dev-instict <felipe.victor875@gmail.com>
2023-09-29 12:17:13 -0300

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTitleComponent,
+    MenuBarComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
